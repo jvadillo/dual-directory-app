78c78ec4185e950bf373e4c6f0267a7dda2dac81
Jon Vadillo <vadillo.jon@gmail.com>
2022-01-14 23:51:26 +0100

    Database creation

--- resources/views/components/alert-messages.blade.php
+++ resources/views/components/alert-messages.blade.php
@@ -1,23 +1,23 @@
 @if(Session::has('success'))
-        <div class="absolute right-5 top-5 h-16 w-96 hideAfter5s">
-            <!-- success message -->
-            <div class="bg-green-100 rounded-md p-3 flex">
-                <svg
-                    class="stroke-2 stroke-current text-green-600 h-8 w-8 mr-2 flex-shrink-0"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                >
-                    <path d="M0 0h24v24H0z" stroke="none" />
-                    <circle cx="12" cy="12" r="9" />
-                    <path d="M9 12l2 2 4-4" />
-                </svg>
+    <div class="absolute right-5 top-5 h-16 w-96 hideAfter5s">
+        <!-- success message -->
+        <div class="bg-green-100 rounded-md p-3 flex">
+            <svg
+                class="stroke-2 stroke-current text-green-600 h-8 w-8 mr-2 flex-shrink-0"
+                viewBox="0 0 24 24"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            >
+                <path d="M0 0h24v24H0z" stroke="none" />
+                <circle cx="12" cy="12" r="9" />
+                <path d="M9 12l2 2 4-4" />
+            </svg>
 
-                <div class="text-green-700">
-                    <div class="font-bold text-xl">{{Session::get('success')}}</div>
-                    <div>La operación se ha realizado correctamente.</div>
-                </div>
+            <div class="text-green-700">
+                <div class="font-bold text-xl">{{Session::get('success')}}</div>
+                <div>La operación se ha realizado correctamente.</div>
             </div>
         </div>
-        @endif
+    </div>
+@endif
