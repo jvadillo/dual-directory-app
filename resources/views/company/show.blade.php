a1593dc0376b5562d555b708e4f67592f137a683
Jon Vadillo <vadillo.jon@gmail.com>
2022-03-14 16:15:23 +0100

    Bug fixed

--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -97,6 +97,7 @@ Detalle de la empresa {{ $company->name }}
               <a target="_blank" href="http://{{ $company->video_url }}" class="text-blue-600 hover:underline">{{ $company->video_url }} <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 inline mb-1"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg></a>
               @else
               <span>No hay videos.</span>
+              @endif
             </div>
           </div>
           <div class="mt-4 bg-white mr-5 w-full shadow-lg overflow-hidden sm:rounded-lg px-6 py-5">
